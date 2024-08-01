cfe4367bb48bafadfeb6b87a33717a5fab3ec893
Yuri Palacio <yurifpalacio@gmail.com>
2024-08-01 14:24:45 -0300

    Implementing pets_repository test

---
+++ src/models/sqlite/repositories/repositories_test.py
@@ -0,0 +1,17 @@
+import pytest
+from src.models.sqlite.settings.connection import db_connection_handler
+from .pets_repository import PetsRepository
+
+# db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason="database interation")
+def test_list_pets():
+  repo = PetsRepository(db_connection_handler)
+  response = repo.list_pets()
+  print(response)
+
+@pytest.mark.skip(reason="database interation")
+def test_delete_pet():
+  name = "belinha"
+  repo = PetsRepository(db_connection_handler)
+  repo.delete_pets(name)
