ae5c0e0c86c194064165d91ec97e9389b9893aba
Yuri Palacio <yurifpalacio@gmail.com>
2024-08-05 09:51:00 -0300

    Implementing people_repository

--- src/models/sqlite/repositories/pets_repository_test.py
+++ src/models/sqlite/repositories/pets_repository_test.py
@@ -1,4 +1,6 @@
 from unittest import mock
+import pytest
+from sqlalchemy.orm.exc import NoResultFound
 from mock_alchemy.mocking import UnifiedAlchemyMagicMock
 from src.models.sqlite.entities.pets import PetsTable
 from .pets_repository import PetsRepository
@@ -23,6 +25,20 @@ class MockConnection:
   def __exit__(self, exc_type, exc_val, exc_tb):
     pass
 
+class MockConnectionNoResult:
+  def __init__(self) -> None:
+    self.session = UnifiedAlchemyMagicMock()
+    self.session.query.side_effect = self.__raise_no_result_found
+
+  def __raise_no_result_found(self, *args, **kwargs):
+    raise NoResultFound("No result found")
+
+  def __enter__(self):
+    return self
+
+  def __exit__(self, exc_type, exc_val, exc_tb):
+    pass
+
 def test_list_pets():
   mock_connection = MockConnection()
   repo = PetsRepository(mock_connection)
@@ -33,3 +49,34 @@ def test_list_pets():
   mock_connection.session.filter.assert_not_called()
 
   assert response[0].name == "dog"
+
+def test_delete_pet():
+  name = 'dog'
+  mock_connection = MockConnection()
+  repo = PetsRepository(mock_connection)
+  repo.delete_pets(name)
+
+  mock_connection.session.query.assert_called_once_with(PetsTable)
+  mock_connection.session.filter.assert_called_once_with(PetsTable.name == name)
+  mock_connection.session.delete.assert_called_once()
+  mock_connection.session.commit.assert_called_once()
+
+def test_list_pets_no_result():
+  mock_connection = MockConnectionNoResult()
+  repo = PetsRepository(mock_connection)
+  response = repo.list_pets()
+
+  mock_connection.session.query.assert_called_once_with(PetsTable)
+  mock_connection.session.all.assert_not_called()
+
+  assert response == []
+
+def test_delete_pet_error():
+  name = 'dog'
+  mock_connection = MockConnectionNoResult()
+  repo = PetsRepository(mock_connection)
+
+  with pytest.raises(Exception):
+    repo.delete_pets(name)
+
+  mock_connection.session.rollback.assert_called_once()
--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,6 +1,7 @@
 import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
+from .people_repository import PeopleRepository
 
 # db_connection_handler.connect_to_db()
 
@@ -15,3 +16,21 @@ def test_delete_pet():
   name = "belinha"
   repo = PetsRepository(db_connection_handler)
   repo.delete_pets(name)
+
+@pytest.mark.skip(reason="database interation")
+def test_insert_person():
+  first_name = "name"
+  last_name = "last name"
+  age = 31
+  pet_id = 2
+
+  repo = PeopleRepository(db_connection_handler)
+  repo.insert_person(first_name, last_name, age, pet_id)
+
+@pytest.mark.skip(reason="database interation")
+def test_get_person():
+  person_id = 1
+
+  repo = PeopleRepository(db_connection_handler)
+  response = repo.get_person(person_id)
+  print(response)
