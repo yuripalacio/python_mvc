b45abadfe270d3e3f5b149df1036fb6dae6a5e2a
Yuri Palacio <yurifpalacio@gmail.com>
2024-08-06 16:33:05 -0300

    Implementing person_creator_view

---
+++ src/views/person_creator_view.py
@@ -0,0 +1,14 @@
+from src.controllers.interfaces.person_creator_controller import PersonCreatorControllerInterface
+from .http_types.http_request import HttpRequest
+from .http_types.http_response import HttpResponse
+from .interfaces.view_interface import ViewInterface
+
+class PersonCreatorView(ViewInterface):
+  def __init__(self, controller: PersonCreatorControllerInterface) -> None:
+    self.__controller = controller
+
+  def handle(self, http_request: HttpRequest) -> HttpResponse:
+    person_info = http_request.body
+    body_response = self.__controller.create(person_info=person_info)
+
+    return HttpResponse(status_code=201, body=body_response)
